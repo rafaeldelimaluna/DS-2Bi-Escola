1543aa9934c5a1773384f0af00eafac370a78b3a
rafaelluna <rafael.luna01@etec.sp.gov.br>
2024-08-05 13:43:54 -0300

    DAO part 1

---
+++ WindowsFormsApp1/DAO/ProfessorDAO.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.SqlClient;
+using Model.Entitidades;
+
+
+namespace WindowsFormsApp1.DAO
+{
+   
+
+    class ProfessorDAO
+    {
+
+        private string LinhaConexao = "SErver=L505MPF;Database=AULA_DS;User Id=sa;Password=admsql";
+        private SqlConnection Conexao;
+      public ProfessorDAO() { 
+        Conexao = new SqlConnection(LinhaConexao);
+        }
+    public void Inserir(ProfessoresEntidade professor)
+    {
+        Conexao.Open();
+        string query = "Insert into professores (Nome,Apleido) VALUES (@nome, @apelido)";
+        SqlCommand comando = new SqlCommand(query, Conexao);
+        SqlParameter parametro1 = new SqlParameter("@nome", professor.Nome);
+        SqlParameter parametro2 = new SqlParameter("@apelido", professor.Apelido);
+        comando.Parameters.Add(parametro1);
+        comando.Parameters.Add(parametro2);
+        comando.ExecuteNonQuery();
+        Conexao.Close();
+    }
+}
